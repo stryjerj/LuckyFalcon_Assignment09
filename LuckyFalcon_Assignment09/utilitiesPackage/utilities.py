a9b14da57f0ed8ed3a3ce720c788a965bb596f94
stryjerj <rstryjewski10@gmail.com>
2024-11-06 21:28:39 -0500

    Documentation

--- LuckyFalcon_Assignment09/utilitiesPackage/utilities.py
+++ LuckyFalcon_Assignment09/utilitiesPackage/utilities.py
@@ -1,11 +1,11 @@
 # *****************************************************
-# Name: Cheikh Abdoul
-# email:  abdoulch@mail.uc.edu
-# Assignment Number: Assignment06
-# Due Date:   11/06/2024 
-# Course #/Section:   001
-# Semester/Year:   fall 2024
-# Brief Description of the assignment: Accessing data from the Grocery Store Simulator database, and produces results.
+# Name: Cheikh Abdoul, Roman Stryjewski
+# email:  abdoulch@mail.uc.edu, stryjerj@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date:   11/07/2024 
+# Course #/Section:   IS 4010 001
+# Semester/Year:   Fall 2024
+# Brief Description of the assignment: Accessing the Grocery Store Simulator database
 # Brief Citations: N/A
 # Anything else that's relevant:
 # *****************************************************
